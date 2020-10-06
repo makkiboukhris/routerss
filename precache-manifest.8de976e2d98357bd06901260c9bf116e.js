17984f0794c78b03e9137339a417385ab4de2ed8
makkiboukhris <makki01boukhris@gmail.com>
2020-10-06 11:16:55 +0100

    Updates

---
+++ precache-manifest.8de976e2d98357bd06901260c9bf116e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ab897cbcbcc2d3a1e37655ea89434f",
+    "url": "/routerss/index.html"
+  },
+  {
+    "revision": "5a04d27997e96fa721bf",
+    "url": "/routerss/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f4b9331ced1f426ebbd2",
+    "url": "/routerss/static/css/main.6452e301.chunk.css"
+  },
+  {
+    "revision": "5a04d27997e96fa721bf",
+    "url": "/routerss/static/js/2.b8cac093.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/routerss/static/js/2.b8cac093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b9331ced1f426ebbd2",
+    "url": "/routerss/static/js/main.933f6b90.chunk.js"
+  },
+  {
+    "revision": "aac73e26512a7936fd90",
+    "url": "/routerss/static/js/runtime-main.73bdd5d5.js"
+  }
+]);
